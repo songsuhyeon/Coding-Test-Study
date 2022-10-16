8a1e3128914a5c64247b3508227ebfddaeb01d3c
songsuhyeon <65274226+songsuhyeon@users.noreply.github.com>
2022-10-17 01:39:12 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 88 ms, Memory: 11836 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,25 +11,26 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		int[] arr = new int[N]; // 수열의 수들
-		int[] LIS = new int[N]; // 동적테이블 : 각 원소를 끝으로하는 LIS 값
+		int[] C = new int[N]; // 동적테이블 C[k]: 해당(k) 길이를 만족하는 자리(k자리)에 오는 수의 최소값
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int max = 0;
-		// 앞쪽부터 모든 원소 기준으로 자신을 끝으로하는 LIS 계산
+		int size = 0;
+
 		for (int i = 0; i < N; i++) {
-			LIS[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (arr[j] < arr[i] && LIS[i] < LIS[j] + 1)
-					LIS[i] = LIS[j] + 1;
-			}
-			max = Math.max(max, LIS[i]);
+			int pos = Arrays.binarySearch(C, 0, size, arr[i]);
+			if (pos >= 0)	continue;  // 배열에 arr[i]가 있는 경우
+
+			// 배열에 arr[i]가 없는 경우
+			int insertPos = Math.abs(pos) - 1; // 맨 뒤 or 기존 원소 대체 자리
+			C[insertPos] = arr[i];
+
+			if (insertPos == size)  size++;
 		}
-		//System.out.println(Arrays.toString(LIS));
-		System.out.println(max);
+		System.out.println(size);
 	}
 
 }
