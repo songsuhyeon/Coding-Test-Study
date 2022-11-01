c0bf952e20229eba3b0f9d231daf9b986a81d9a1
songsuhyeon <65274226+songsuhyeon@users.noreply.github.com>
2022-11-02 00:34:23 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int T;
+	static long[] arr = new long[101];
+
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+
+		arr[1] = 1;
+		arr[2] = 1;
+		arr[3] = 1;
+		arr[4] = 2;
+		arr[5] = 2;
+
+		for (int i = 6; i <= 100; i++) {
+			arr[i] = arr[i - 1] + arr[i - 5];
+		}
+
+		for (int i = 0; i < T; i++) {
+			sb.append(arr[(int) Long.parseLong(br.readLine())]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
