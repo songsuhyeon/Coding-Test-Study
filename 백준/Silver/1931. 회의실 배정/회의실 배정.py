b06df8b0ea14defb0b1edc5b20716bc5b514cb3e
songsuhyeon <65274226+songsuhyeon@users.noreply.github.com>
2022-12-25 05:47:30 +0900

    [Silver I] Title: 회의실 배정, Time: 264 ms, Memory: 53496 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,24 +1,24 @@
 import sys
 
+# 회의가 끝나는 시간이 짧은 것 고르기
+# 끝난 시간 이후부터 가장 빨리 시작되는 회의 고르기
 n = int(input())
+time = []
 
-L = []
-for i in range(n):
-    start, finish = map(int, sys.stdin.readline().split())
-    L.append([start, finish])
+for _ in range(n):
+    s, e = map(int, sys.stdin.readline().split())
+    time.append([s, e])
 
-# 가장 빨리 끝나는 순으로 정렬
-L.sort(key=lambda x:[x[1], x[0]])
+# x[1] (회의가 끝나는 시간)을 기준으로 오름차순
+# 회의가 끝나는 시간이 같다면, x[0] (회의 시작 시간)을 기준으로 오름차순
+time.sort(key=lambda x:(x[1], x[0]))
 
-s_time = L[0][0]
-f_time = L[0][1]
+answer = 0
+endTime = 0
 
-count = 1
+for i in range(len(time)):
+    if endTime <= time[i][0]:
+        endTime = time[i][1]
+        answer += 1
 
-for i in range(1, n):
-    if f_time <= L[i][0]:
-        s_time = L[i][0]
-        f_time = L[i][1]
-        count += 1
-        
-print(count)
+print(answer)
