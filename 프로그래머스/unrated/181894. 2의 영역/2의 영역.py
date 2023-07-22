6bfe9b3a8f0879f5d66f1f74d7135603acdc8905
Song SuHyeon <65274226+songsuhyeon@users.noreply.github.com>
2023-07-23 07:58:17 +0900

    [unrated] Title: 2의 영역, Time: 0.29 ms, Memory: 15.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+    try: 
+        start = arr.index(2)
+    except:
+        return [-1]
+    
+    for i in range(len(arr)-1, 0, -1):
+        if arr[i] == 2:
+            end = i
+            break
+    
+    return arr[start:end+1]
